61c015a8a329efc6702552ac4011b86d947aeea4
vandeppce <sdtczyj@163.com>
2022-04-28 23:54:40 +0800

    2022/04/28

--- binary tree/226InvertTree.py
+++ binary tree/226InvertTree.py
@@ -17,3 +17,47 @@ class Solution:
             invert(root.right)
         invert(root)
         return root
+
+"""
+# 二刷，带返回值
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return root
+        
+        leftNode = self.invertTree(root.right)
+        rightNode = self.invertTree(root.left)
+
+        root.left = leftNode
+        root.right = rightNode
+        return root
+"""
+
+"""
+二刷，迭代，前序遍历
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return root
+        stack = [root]
+        while stack:
+            node = stack.pop(0)
+            node.left, node.right = node.right, node.left
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return root
+"""
