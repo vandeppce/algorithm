b4da862bb145d05c63ba0263e9501ad9f2566cdb
vandeppce <sdtczyj@163.com>
2022-05-06 00:16:22 +0800

    2022/05/05

--- greedy/714MaxProfit.py
+++ greedy/714MaxProfit.py
@@ -9,5 +9,21 @@
 
 prices = [1, 3, 2, 8, 4, 9]
 fee = 2
-
+prices = [1,3,7,5,10,3]
+fee = 3
+# 贪心算法，分三种情况讨论，注意，else中的是计算子区间内的收益，
+# 若遇到更大的收益会更新，因为为了防止多减去fee，因此prices[i]需要减fee
 def maxProfit(prices, fee):
+    minPrice = prices[0]
+    total = 0
+    for i in range(1, len(prices)):
+        if prices[i] < minPrice:
+            minPrice = prices[i]
+        elif prices[i] < minPrice + fee:
+            continue
+        else:
+            total += prices[i] - minPrice - fee
+            minPrice = prices[i] - fee
+    return total
+
+print(maxProfit(prices, fee))
